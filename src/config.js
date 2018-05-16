14ee1344e583296ae6d2befd2be475ee29f7ba19
Rayi Yanu Tara <rayi.yanu.tara@gmail.com>
2018-05-16 07:51:12 +0000

    config.js edited online with Bitbucket --> url production

--- src/config.js
+++ src/config.js
@@ -1,8 +1,18 @@
-export const PRODUCTS_ENDPOINT_URL = 'https://product-hub-testing.azurewebsites.net/graphql'
-export const ORDERING_ENDPOINT_URL = 'https://ordering-service-testing.azurewebsites.net/graphql'
-export const IAM_ENDPOINT_URL = 'https://iam-message-testing.azurewebsites.net'
-export const CHAT_ENDPOINT_URL = 'https://iam-message-testing.azurewebsites.net/chatql'
-export const USER_ENDPOINT_URL = 'https://iam-message-testing.azurewebsites.net/userql'
+const prodSetup = {
+  iam : 'https://api.blanja.tw/iam',
+  productql : 'https://api.blanja.tw/productql',
+  orderql : 'https://api.blanja.tw/orderql',
+  chatql : 'https://api.blanja.tw/chatql',
+  userql : 'https://api.blanja.tw/userql'
+}
+
+const isProd = window.location.host.match(/^(?:www.)?blanja\.(?:tw|hk)/gi);
+
+export const PRODUCTS_ENDPOINT_URL = isProd ? prodSetup.productql : 'https://product-hub-testing.azurewebsites.net/graphql'
+export const ORDERING_ENDPOINT_URL = isProd ? prodSetup.orderql : 'https://ordering-service-testing.azurewebsites.net/graphql'
+export const IAM_ENDPOINT_URL = isProd ? prodSetup.iam : 'https://iam-message-testing.azurewebsites.net'
+export const CHAT_ENDPOINT_URL = isProd ? prodSetup.chatql : 'https://iam-message-testing.azurewebsites.net/chatql'
+export const USER_ENDPOINT_URL = isProd ? prodSetup.userql :' https://iam-message-testing.azurewebsites.net/userql'
 
 export const getIAMEndpoint = params => `${IAM_ENDPOINT_URL}${params || ''}`
 
@@ -10,6 +20,8 @@ export const API_TOKEN_STORAGE_URI = 'hashAPIToken'
 export const AUTHORIZATION_TOKEN_STORAGE_URI = 'hashAuthToken'
 
 export const CART_STORAGE_URI = 'cart'
+
+
 
 export default {
   PRODUCTS_ENDPOINT_URL,
