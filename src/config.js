8dd17e7d8aa178c5fc0efc799154a51725ebb4df
Yahya Sahaja <yahya.sahaja2@gmail.com>
2018-05-16 16:09:53 +0700

    update config

--- src/config.js
+++ src/config.js
@@ -1,15 +1,38 @@
-export const PRODUCTS_ENDPOINT_URL = 'https://product-hub-testing.azurewebsites.net/graphql'
-export const ORDERING_ENDPOINT_URL = 'https://ordering-service-testing.azurewebsites.net/graphql'
-export const IAM_ENDPOINT_URL = 'https://iam-message-testing.azurewebsites.net'
-export const CHAT_ENDPOINT_URL = 'https://iam-message-testing.azurewebsites.net/chatql'
-export const USER_ENDPOINT_URL = 'https://iam-message-testing.azurewebsites.net/userql'
+let PRODUCTS_ENDPOINT_URL = 'https://product-hub-testing.azurewebsites.net/graphql'
+let ORDERING_ENDPOINT_URL = 'https://ordering-service-testing.azurewebsites.net/graphql'
+let IAM_ENDPOINT_URL = 'https://iam-message-testing.azurewebsites.net'
+let CHAT_ENDPOINT_URL = 'https://iam-message-testing.azurewebsites.net/chatql'
+let USER_ENDPOINT_URL = 'https://iam-message-testing.azurewebsites.net/userql'
 
-export const getIAMEndpoint = params => `${IAM_ENDPOINT_URL}${params || ''}`
+let getIAMEndpoint = params => `${IAM_ENDPOINT_URL}${params || ''}`
 
-export const API_TOKEN_STORAGE_URI = 'hashAPIToken'
-export const AUTHORIZATION_TOKEN_STORAGE_URI = 'hashAuthToken'
+let API_TOKEN_STORAGE_URI = 'hashAPIToken'
+let AUTHORIZATION_TOKEN_STORAGE_URI = 'hashAuthToken'
 
-export const CART_STORAGE_URI = 'cart'
+let CART_STORAGE_URI = 'cart'
+
+if (process.env.NODE_ENV === 'production') {
+  PRODUCTS_ENDPOINT_URL = 'https://api.blanja.tw/productql'
+  ORDERING_ENDPOINT_URL = 'https://api.blanja.tw/orderql'
+  IAM_ENDPOINT_URL = 'https://api.blanja.tw/iam'
+  CHAT_ENDPOINT_URL = 'https://api.blanja.tw/chatql'
+  USER_ENDPOINT_URL = 'https://api.blanja.tw/userql'
+}
+
+export {
+  PRODUCTS_ENDPOINT_URL,
+  ORDERING_ENDPOINT_URL,
+  IAM_ENDPOINT_URL,
+  CHAT_ENDPOINT_URL,
+  USER_ENDPOINT_URL,
+
+  getIAMEndpoint,
+
+  API_TOKEN_STORAGE_URI,
+  AUTHORIZATION_TOKEN_STORAGE_URI,
+
+  CART_STORAGE_URI,
+}
 
 export default {
   PRODUCTS_ENDPOINT_URL,
