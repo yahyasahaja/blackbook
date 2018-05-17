572165a4b542b8366598eaf5c05df273e7636237
Yahya Sahaja <yahya.sahaja2@gmail.com>
2018-05-17 19:29:38 +0700

    test v5.1

--- public/sw.js
+++ public/sw.js
@@ -113,7 +113,7 @@ function isPublicPath(path) {
 function fetchData(event) {
   let url = event.request.url
   let path = new URL(url).pathname
-  if (path.indexOf('/sw.js') === 0 || path.indexOf('/service-worker.js') === 0) return fetch(event.request)
+  // if (path.indexOf('/sw.js') === 0 || path.indexOf('/service-worker.js') === 0) return fetch(event.request)
 
   if (!isAPI(url) && !isPublicPath(path))
     for (let i in routers) if (checkRoute(path, routers[i])) {
