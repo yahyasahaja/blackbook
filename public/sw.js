6180c8a3c918f508cf9c9bc514e258fc69f3ec59
Yahya Sahaja <yahya.sahaja2@gmail.com>
2018-05-17 20:29:13 +0700

    test 5.5

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 // Set this to true for production
 var doCache = self.location.hostname.indexOf('localhost') === -1
 
-// importScripts('/service-worker.js')
+importScripts('/service-worker.js')
 
 // Name our cache
 var CACHE_NAME = 'build_cache_name' + 'buyer-pwa'
@@ -45,166 +45,166 @@ function registerCaches() {
     })
 }
 
-// var urlConfig = {
-//   prod: {
-//     iam : 'https://api.blanja.tw/iam',
-//     productql : 'https://api.blanja.tw/productql',
-//     orderql : 'https://api.blanja.tw/orderql',
-//     chatql : 'https://api.blanja.tw/chatql',
-//     userql : 'https://api.blanja.tw/userql',
-//   },
-//   dev: {
-//     productql: 'https://product-hub-testing.azurewebsites.net/graphql',
-//     orderql : 'https://ordering-service-testing.azurewebsites.net/graphql',
-//     iam : 'https://iam-message-testing.azurewebsites.net',
-//     chatql : 'https://iam-message-testing.azurewebsites.net/chatql',
-//     userql :' https://iam-message-testing.azurewebsites.net/userql',
-//   }
-// }
+var urlConfig = {
+  prod: {
+    iam : 'https://api.blanja.tw/iam',
+    productql : 'https://api.blanja.tw/productql',
+    orderql : 'https://api.blanja.tw/orderql',
+    chatql : 'https://api.blanja.tw/chatql',
+    userql : 'https://api.blanja.tw/userql',
+  },
+  dev: {
+    productql: 'https://product-hub-testing.azurewebsites.net/graphql',
+    orderql : 'https://ordering-service-testing.azurewebsites.net/graphql',
+    iam : 'https://iam-message-testing.azurewebsites.net',
+    chatql : 'https://iam-message-testing.azurewebsites.net/chatql',
+    userql :' https://iam-message-testing.azurewebsites.net/userql',
+  }
+}
 
-// let routers = ['/category', '/product', '/home', '/promo', '/favorite', '/chat', '/account']
+let routers = ['/category', '/product', '/home', '/promo', '/favorite', '/chat', '/account']
 
-// // When the webpage goes to fetch files, we intercept that request and serve up the matching files
-// // if we have them
-// let htmlLocation = new URL('index.html', self.location).toString()
-// var finalHTMLLocation
+// When the webpage goes to fetch files, we intercept that request and serve up the matching files
+// if we have them
+let htmlLocation = new URL('index.html', self.location).toString()
+var finalHTMLLocation
 
-// if (doCache)
-//   finalHTMLLocation = urlsToCacheKeys.get(htmlLocation)
-// else
-//   finalHTMLLocation = htmlLocation
+if (doCache)
+  finalHTMLLocation = urlsToCacheKeys.get(htmlLocation)
+else
+  finalHTMLLocation = htmlLocation
 
-// //console.log(htmlLocation, hashedHTMLLocation)
+//console.log(htmlLocation, hashedHTMLLocation)
 
-// var publicPath = [
-//   '/static',
-//   '/favicon.ico',
-//   '/manifest.json',
-//   '/sw.js',
-//   '/web.config',
-//   '/service-worker.js',
-//   '/asset-manifest.json',
-// ]
+var publicPath = [
+  '/static',
+  '/favicon.ico',
+  '/manifest.json',
+  '/sw.js',
+  '/web.config',
+  '/service-worker.js',
+  '/asset-manifest.json',
+]
 
-// function checkRoute(path, router) {
-//   if (path.indexOf(router) !== 0) return false
+function checkRoute(path, router) {
+  if (path.indexOf(router) !== 0) return false
 
-//   if (path.length > router.length)
-//     return path[router.length] === '/'
+  if (path.length > router.length)
+    return path[router.length] === '/'
 
   
-//   return true
-// }
+  return true
+}
 
-// function isAPI(url) {
-//   for (var i in urlConfig) for (var j in urlConfig[i]) if (url === urlConfig[i][j]) {
-//     console.log(url, ' is public url')
-//     return true
-//   }
-// }
+function isAPI(url) {
+  for (var i in urlConfig) for (var j in urlConfig[i]) if (url === urlConfig[i][j]) {
+    console.log(url, ' is public url')
+    return true
+  }
+}
 
-// function isPublicPath(path) {
-//   for (var p of publicPath) if (path.indexOf(p) === 0) {
-//     console.log(path, ' is public path')
-//     return true
-//   }
-// }
+function isPublicPath(path) {
+  for (var p of publicPath) if (path.indexOf(p) === 0) {
+    console.log(path, ' is public path')
+    return true
+  }
+}
 
-// function fetchData(event) {
-//   let url = event.request.url
-//   let path = new URL(url).pathname
+function fetchData(event) {
+  let url = event.request.url
+  let path = new URL(url).pathname
+  fetch(event.request)
+  // if (path.indexOf('/sw.js') === 0 || path.indexOf('/service-worker.js') === 0) return fetch(event.request)
+
+  if (!isAPI(url) && !isPublicPath(path))
+    for (let i in routers) if (checkRoute(path, routers[i])) {
+      //console.log('DI FETCH', hashedHTMLLocation)
+      return caches.match(finalHTMLLocation)
+    }
   
-//   // if (path.indexOf('/sw.js') === 0 || path.indexOf('/service-worker.js') === 0) return fetch(event.request)
+  return caches.match(event.request).then(function (response) {
+    //response can be undefined if no cache found
+    // if (response) {
+    //   console.log('result:', event.request, response)
+    //   caches.open(CACHE_NAME)
+    //   .then(function (cache) {
+    //     cache.keys().then(function(keys) {
+    //       console.log('CACHE: ', keys)
+    //     })
+    //   })
+    // }
 
-//   if (!isAPI(url) && !isPublicPath(path))
-//     for (let i in routers) if (checkRoute(path, routers[i])) {
-//       //console.log('DI FETCH', hashedHTMLLocation)
-//       return caches.match(finalHTMLLocation)
-//     }
-  
-//   return caches.match(event.request).then(function (response) {
-//     //response can be undefined if no cache found
-//     // if (response) {
-//     //   console.log('result:', event.request, response)
-//     //   caches.open(CACHE_NAME)
-//     //   .then(function (cache) {
-//     //     cache.keys().then(function(keys) {
-//     //       console.log('CACHE: ', keys)
-//     //     })
-//     //   })
-//     // }
+    //console.log(path, response)
+    return response || fetch(event.request)
+  }).catch(err => {
+    console.log('error', event.request, err)
+  })
+}
 
-//     //console.log(path, response)
-//     return response || fetch(event.request)
-//   }).catch(err => {
-//     console.log('error', event.request, err)
-//   })
-// }
+self.addEventListener('fetch', function (event) {
+  if (doCache) return event.respondWith(fetchData(event))
+})
 
-// self.addEventListener('fetch', function (event) {
-//   if (doCache) return event.respondWith(fetchData(event))
-// })
-
-// // self.addEventListener('push', function(event) {
-// //   var payload = event.data ? event.data.text() : 'no payload'
-// //   event.waitUntil(
-// //     self.registration.showNotification('New Notification', {
-// //       body: payload
-// //     })
-// //   )
-// // })
-
-// self.addEventListener('push', function (event) {
-//   // var options = {
-//   //   body: 'This notification was generated from a push!',
-//   //   icon: 'images/example.png',
-//   //   vibrate: [100, 50, 100],
-//   //   data: {
-//   //     dateOfArrival: Date.now(),
-//   //     primaryKey: '2'
-//   //   },
-//   //   actions: [
-//   //     {
-//   //       action: 'explore', title: 'Explore this new world',
-//   //       icon: 'images/checkmark.png'
-//   //     },
-//   //     {
-//   //       action: 'close', title: 'Close',
-//   //       icon: 'images/xmark.png'
-//   //     },
-//   //   ]
-//   // }
-
-//   //console.log(event)
-//   let context = {}
-
-//   if (event.data) {
-//     try {
-//       context = JSON.parse(event.data.text())
-//     } catch (e) {
-//       context = { body: event.data.text() }
-//     }
-//   }
-//   console.log('CONTEXT FROM PUSH LISTENER', context)
-//   ///// broadcast to Window
-//   if (context.body.includes('pesan') || context.threadId) {
-//     ///
-//     this.clients.matchAll({ includeUncontrolled: true }).then(function (allClients) {
-//       allClients.forEach(function (client) {
-//         client.postMessage({
-//           threadId: context.threadId,
-//           body: context.body
-//         })
-//       })
+// self.addEventListener('push', function(event) {
+//   var payload = event.data ? event.data.text() : 'no payload'
+//   event.waitUntil(
+//     self.registration.showNotification('New Notification', {
+//       body: payload
 //     })
-//   }
-//   ///
-//   const title = context.title || 'Blanja-Commerce'
-//   const options = {
-//     body: context.body || 'Pesan baru dari Blanja',
-//     icon: '/static/img/icons/android-chrome-192x192.png',
-//     vibrate: [100, 50, 100]
-//   }
-
-//   event.waitUntil(self.registration.showNotification(title, options))
+//   )
 // })
+
+self.addEventListener('push', function (event) {
+  // var options = {
+  //   body: 'This notification was generated from a push!',
+  //   icon: 'images/example.png',
+  //   vibrate: [100, 50, 100],
+  //   data: {
+  //     dateOfArrival: Date.now(),
+  //     primaryKey: '2'
+  //   },
+  //   actions: [
+  //     {
+  //       action: 'explore', title: 'Explore this new world',
+  //       icon: 'images/checkmark.png'
+  //     },
+  //     {
+  //       action: 'close', title: 'Close',
+  //       icon: 'images/xmark.png'
+  //     },
+  //   ]
+  // }
+
+  //console.log(event)
+  let context = {}
+
+  if (event.data) {
+    try {
+      context = JSON.parse(event.data.text())
+    } catch (e) {
+      context = { body: event.data.text() }
+    }
+  }
+  console.log('CONTEXT FROM PUSH LISTENER', context)
+  ///// broadcast to Window
+  if (context.body.includes('pesan') || context.threadId) {
+    ///
+    this.clients.matchAll({ includeUncontrolled: true }).then(function (allClients) {
+      allClients.forEach(function (client) {
+        client.postMessage({
+          threadId: context.threadId,
+          body: context.body
+        })
+      })
+    })
+  }
+  ///
+  const title = context.title || 'Blanja-Commerce'
+  const options = {
+    body: context.body || 'Pesan baru dari Blanja',
+    icon: '/static/img/icons/android-chrome-192x192.png',
+    vibrate: [100, 50, 100]
+  }
+
+  event.waitUntil(self.registration.showNotification(title, options))
+})
