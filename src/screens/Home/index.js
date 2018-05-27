f8f490056ed0259f82ab0f7f573e41ca4aa953b9
Yahya Sahaja <yahya.sahaja2@gmail.com>
2018-05-27 20:23:59 +0700

    login using secretstring :sparkles:

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -76,6 +76,7 @@ class Home extends Component {
         isFetchDisabled: this.state.products.length === activePromoteds.totalCount
       })
     } else if (newError) {
+      // return console.log(newError)
       tokens.refetchAPIToken().then(() => window.location.reload())
     }
   }
