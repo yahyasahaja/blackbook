9e35a4e30488a52332a6a2901f8e2894142c9c92
alfari <alif.anshory@yahoo.com>
2018-05-30 16:03:24 +0700

    sending chat test

--- cypress/integration/07_chat.spec.js
+++ cypress/integration/07_chat.spec.js
@@ -2,8 +2,21 @@ describe('Chat page', () => {
   it('Visits the chat page', () => {
     cy.visit('/')
     cy.url().should('include', '/home')
-
+    cy.login()
     cy.get('a[href="/chat"]').click()
     cy.url().should('include', '/chat')
+  })
+
+  it('Chat with seller from chat page', () => {
+    cy.get('a[href="/chat"]').click()
+    cy.get('[class^="thread-item"]').eq(0).click()
+    cy.get('[class^="conversation--bubble"]').then(item => {
+      const dateNow = 'Test chat on '+new Date().toString()
+      cy.get('textarea').type(dateNow)
+      cy.spy(window,'fetch')
+      cy.get('textarea').siblings('button').click()
+      cy.wait(1000)
+      cy.get('[class^="conversation--bubble"]').last().should('contain', dateNow)
+    })
   })
 })
