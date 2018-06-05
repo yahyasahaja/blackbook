8402f69b5bb68c0dbde9e378eb527068df26e82b
Made Baruna <made.setia@gmail.com>
2018-06-05 16:03:38 +0700

    Lepas apollo hoc from chat

--- src/screens/Chat/Conversation.js
+++ src/screens/Chat/Conversation.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
-import { graphql, compose } from 'react-apollo'
 import gql from 'graphql-tag'
 import { observer } from 'mobx-react'
+import { observe } from 'mobx'
 import { Link } from 'react-router-dom'
 import ProgressBar from 'react-toolbox/lib/progress_bar'
 import moment from 'moment'
 import PopupBar, { ANIMATE_HORIZONTAL } from '../../components/PopupBar'
 import ChatBubble from '../../components/Chat/ChatBubble'
-import client from '../../services/graphql/chatClient'
+import productClient from '../../services/graphql/productClient'
+import chatClient from '../../services/graphql/chatClient'
+import { convertToMoneyFormat } from '../../utils'
 
 import styles from './css/conversation.scss'
 import loadingTheme from './css/loading.scss'
 import loadingSubmitTheme from './css/loading-submit.scss'
 
-import { appStack, onlineStatus, badges, chat, user } from '../../services/stores'
+import {
+  appStack,
+  badges,
+  chat,
+  user,
+} from '../../services/stores'
 
 @observer
 class Conversation extends Component {
@@ -29,48 +36,32 @@ class Conversation extends Component {
         ? this.props.location.state.sender
         : 'Penjual',
     message: '',
+    messages: [],
+    product: null,
+    loadingProduct: true,
+    loadingMessage: true,
+    loadingIncomingMessage: true,
     loading: false,
     showed: false,
     scrolledToBottom: false,
-    refetchSending: false,
     gettingOlderMessage: false,
+    error: '',
   }
 
-  componentWillReceiveProps(nextProps) {
-    if(!user.isLoggedIn) {
-      return this.props.history.replace('/account')
-    }
-
-    if (nextProps.data && !nextProps.data.loading) {
-      this.setState({ showed: true })
-    }
-
-    if (
-      this.props.data &&
-      !this.props.data.loading &&
-      this.props.match.params.id !== 'new'
-    ) {
-      if (!this.state.showed && onlineStatus.isOnline) {
-        this.props.data.refetch()
-        this.scrollToBottom()
+  async componentWillMount() {
+    this.getThreadId()
+    this.disposer = observe(user, 'isLoading', () => {
+      if (!user.isLoading) {
+        this.getThreadId()
       }
-    }
-
-    if (nextProps.product && nextProps.product.product) {
-      this.setState({ title: nextProps.product.product.seller.name })
-    }
-
-    if (nextProps.threadId) {
-      if (nextProps.threadId.thread && nextProps.threadId.thread.id !== null) {
-        nextProps.history.replace(`/chat/${nextProps.threadId.thread.id}`)  
-      }
-    }
-    
+    })
   }
 
   async componentDidMount() {
-    if(this.props.location.state.index) {
-      chat.threads[this.props.location.state.index].isRead = true
+    if (this.props.location.state.index) {
+      if (chat.threads[this.props.location.state.index]) {
+        chat.threads[this.props.location.state.index].isRead = true
+      }
     }
     window.addEventListener('scroll', this.checkScroll)
     window.addEventListener('gesturechange', this.checkScroll)
@@ -80,7 +71,7 @@ class Conversation extends Component {
         e.type === 'message' &&
         e.data.threadId === Number(this.props.match.params.id)
       ) {
-        this.refetchMessage()
+        this.getMessages(undefined, true)
       }
     }
 
@@ -93,16 +84,6 @@ class Conversation extends Component {
     window.removeEventListener('gesturechange', this.checkScroll)
   }
 
-  componentDidUpdate() {
-    if (
-      !this.state.scrolledToBottom &&
-      this.props.data &&
-      !this.props.data.loading
-    ) {
-      this.scrollToBottom()
-    }
-  }
-
   checkScroll = async () => {
     let scrollPosition = Math.max(
       document.documentElement.scrollTop,
@@ -111,15 +92,18 @@ class Conversation extends Component {
 
     if (
       scrollPosition < 10 &&
-      this.props.data &&
-      !this.props.data.loading &&
-      (this.props.data.thread && this.props.data.thread.messages.total !== 0) &&
+      this.state.messages &&
+      !this.state.loadingMessage &&
+      this.state.messages.total !== 0 &&
       !this.state.gettingOlderMessage
     ) {
       this.setState({ gettingOlderMessage: true })
 
       const currentTopMessage = this.messagesRef[0]
-      await this.props.data.fetchMore()
+      // fetch more message
+      await this.getMessages(
+        this.state.messages.data[this.state.messages.data.length - 1].id,
+      )
       currentTopMessage.scrollIntoView()
       window.scrollBy(0, -180)
 
@@ -128,8 +112,113 @@ class Conversation extends Component {
   }
 
   scrollToBottom() {
+    console.log('scroll to bottom')
     this.messagesEnd.scrollIntoView({ behavior: 'auto' })
     this.setState({ scrolledToBottom: true })
+  }
+
+  async getThreadId() {
+    // check if user logged in
+    if (!user.isLoading && !user.isLoggedIn) {
+      return this.props.history.replace('/account')
+    } else {
+      // fetch thread id if new
+      if (this.props.match.params.id === 'new') {
+        const {
+          data: { thread },
+        } = await chatClient.query({
+          query: getThreadId,
+          variables: {
+            productId: this.props.location.state.productId,
+          },
+        })
+
+        if (thread !== null) {
+          return this.props.history.replace(`/chat/${thread.id}`)
+        } else {
+          this.getProduct(this.props.location.state.productId)
+        }
+      }
+
+      this.getMessages()
+    }
+  }
+
+  async getProduct(productId) {
+    try {
+      const {
+        data: { product },
+      } = await productClient.query({
+        query: getProductQuery,
+        variables: {
+          id: productId,
+        },
+      })
+
+      this.setState({
+        loadingProduct: false,
+        product,
+      })
+    } catch (err) {
+      this.setState({
+        loadingProduct: false,
+        error: 'Tidak dapat mengambil informasi produk',
+      })
+    }
+  }
+
+  async getMessages(before = undefined, fromNotification = false) {
+    console.log('fetching messages', before)
+
+    // get messages
+    try {
+      this.setState({ 
+        loadingMessage: !fromNotification,
+        loadingIncomingMessage: fromNotification 
+      })
+
+      const {
+        data: { thread },
+      } = await chatClient.query({
+        fetchPolicy: 'network-only',
+        query: getMessagesQuery,
+        variables: {
+          id: this.props.match.params.id,
+          before,
+        },
+      })
+
+      if (before) {
+        // append message
+        this.setState({
+          loadingMessage: false,
+          messages: {
+            total: thread.messages.total,
+            data: [...this.state.messages.data, ...thread.messages.data],
+          },
+        })
+      } else {
+        this.setState({
+          loadingMessage: false,
+          loadingIncomingMessage: false,
+          messages: thread.messages,
+        })
+
+        if (!fromNotification) this.scrollToBottom()
+      }
+
+      // after get message, get product detail
+      if (!before && !this.state.product) this.getProduct(thread.productId)
+      return true
+    } catch (err) {
+      console.log(err)
+      this.setState({
+        loadingMessage: false,
+        error: 'Tidak dapat mengambil chat',
+      })
+
+      return false
+    }
   }
 
   handleInput(event) {
@@ -139,67 +228,9 @@ class Conversation extends Component {
       Math.min(this.messageInput.scrollHeight, 300) + 'px'
   }
 
-  convertToMoneyFormat(num, currency) {
-    let res = num
-      .toString()
-      .split('')
-      .reverse()
-      .reduce(function(acc, num, i) {
-        return num == '-' ? acc : num + (i && !(i % 3) ? '.' : '') + acc
-      }, '')
-
-    return `${currency} ${res}`
-  }
-
-  async sendMessage() {
-    const { message, loading } = this.state
-    if (message.length === 0 || loading) return
-
-    this.setState({ loading: true, refetchSending: true })
-
-    const { data } = this.props
-    // const { data: res } = await submit(message)
-    const { data: res } = await client.mutate({
-      mutation: sendMessageMutation,
-      variables: {
-        id:
-          this.props.match.params.id === 'new'
-            ? -1
-            : this.props.match.params.id,
-        message,
-        productId: this.props.location.state ? this.props.location.state.productId : null,
-      },
-    })
-
-    if (!res.error) {
-      // clear input
-      this.setState({ message: '', loading: false })
-      // reset height
-      this.messageInput.style.height = ''
-      // refetch the message
-
-      // check if new message
-      if (this.props.match.params.id === 'new') {
-        // if new message change history to id
-        this.setState({ scrolledToBottom: false })
-        this.props.history.replace(`/chat/${res.pushMessage.threadId}`)
-      } else {
-        // if not new message, refetch
-        await data.refetch()
-        this.scrollToBottom()
-        this.setState({ refetchSending: false })
-      }
-    }
-  }
-
-  async refetchMessage() {
-    this.setState({ refetchSending: true })
-    this.props.data.refetch()
-    this.setState({ refetchSending: false })
-  }
-
-  renderMessages(messages) {
-    return messages.reverse().map((message, index) => {
+  renderMessages() {
+    const messages = this.state.messages.data.slice().reverse()
+    return messages.map((message, index) => {
       let isOtherDay = false
 
       if (index === 0) {
@@ -233,9 +264,57 @@ class Conversation extends Component {
     })
   }
 
+  async sendMessage() {
+    const { message, loading } = this.state
+    if (message.length === 0 || loading) return
+
+    this.setState({ loading: true })
+
+    const { data: res } = await chatClient.mutate({
+      mutation: sendMessageMutation,
+      variables: {
+        id:
+          this.props.match.params.id === 'new'
+            ? -1
+            : this.props.match.params.id,
+        message,
+        productId: this.props.location.state ? this.props.location.state.productId : null,
+      },
+    })
+
+    if (!res.error) {
+      // clear input
+      this.setState({ message: '', loading: false })
+      // reset height
+      this.messageInput.style.height = ''
+      // refetch the message
+
+      // check if new message
+      if (this.props.match.params.id === 'new') {
+        // if new message change history to id
+        this.setState({ scrolledToBottom: false })
+        this.props.history.replace(`/chat/${res.pushMessage.threadId}`)
+      } else {
+        // if not new message, refetch
+        await this.getMessages(undefined, true)
+        this.scrollToBottom()
+      }
+    }
+  }
+
   render() {
-    const { message, title, loading } = this.state
-    const { product } = this.props
+    if (user.isLoading) return <div />
+
+    const {
+      loadingProduct,
+      loadingMessage,
+      loadingIncomingMessage,
+      gettingOlderMessage,
+      product,
+      message,
+      title,
+      loading,
+    } = this.state
     return (
       <PopupBar
         onBack={e => {
@@ -253,30 +332,28 @@ class Conversation extends Component {
         }}
       >
         <Link
-          onClick={e => (!product || product.loading) && e.preventDefault()}
-          to={`${product ? (product.loading ? '' : `/product/${product.product.id}`) : ''}`}
+          onClick={e => (!product || loadingProduct) && e.preventDefault()}
+          to={`${
+            product ? (loadingProduct ? '' : `/product/${product.id}`) : ''
+          }`}
         >
           <div className={styles.productBar}>
             <div className={styles.imagePlaceholder}>
               {product &&
-                (!product.loading && (
-                  <img src={product.product.images[0].url} />
-                ))}
+                (!loadingProduct && <img src={product.images[0].url} />)}
             </div>
             <div className={styles.content}>
               <p>
-                {product && !product.loading
-                  ? product.product.name
-                  : 'Memuat produk...'}
+                {product && !loadingProduct ? product.name : 'Memuat produk...'}
               </p>
               {product ? (
-                product.loading ? (
+                loadingProduct ? (
                   <span>memuat...</span>
                 ) : (
                   <span>
-                    {this.convertToMoneyFormat(
-                      product.product.price.value,
-                      product.product.price.currency,
+                    {convertToMoneyFormat(
+                      product.price.value,
+                      product.price.currency,
                     )}
                   </span>
                 )
@@ -287,20 +364,17 @@ class Conversation extends Component {
             <span className={`mdi mdi-chevron-right ${styles.chevronRight}`} />
           </div>
         </Link>
-        {(this.props.data &&
-          this.props.data.loading &&
-          !this.state.refetchSending) ||
-        this.state.gettingOlderMessage ? (
-            <div className={styles.loading}>
-              <ProgressBar theme={loadingTheme} mode="indeterminate" />
-            </div>
-          ) : (
-            ''
-          )}
+        {loadingMessage || loadingIncomingMessage ? (
+          <div className={styles.loading}>
+            <ProgressBar theme={loadingTheme} mode="indeterminate" />
+          </div>
+        ) : (
+          ''
+        )}
         <div className={styles.messages}>
-          {this.props.data &&
-            this.props.data.thread &&
-            this.renderMessages(this.props.data.thread.messages.data.slice())}
+          {(!this.state.loadingMessage || gettingOlderMessage) &&
+            this.state.messages &&
+            this.renderMessages()}
           <div
             style={{ float: 'left', clear: 'both' }}
             ref={el => {
@@ -338,10 +412,6 @@ class Conversation extends Component {
     )
   }
 }
-
-// Conversation.defaultProps = {
-//   data: {},
-// }
 
 const getMessagesQuery = gql`
   query MessageQuery($id: Int!, $before: Int) {
@@ -402,105 +472,4 @@ const sendMessageMutation = gql`
   }
 `
 
-export default compose(
-  // graphql(sendMessageMutation, {
-  //   props: ({ mutate, ownProps }) => ({
-  //     submit: message =>
-  //       mutate({
-  //         variables: {
-  //           id:
-  //             ownProps.match.params.id === 'new'
-  //               ? -1
-  //               : ownProps.match.params.id,
-  //           message,
-  //           productId: ownProps.location.state.productId,
-  //         },
-  //       }),
-  //   }),
-  //   options: {
-  //     client,
-  //   },
-  // }),
-  graphql(getMessagesQuery, {
-    skip: props => props.match.params.id === 'new',
-    options: props => ({
-      client,
-      variables: {
-        id: props.match.params.id,
-      },
-      fetchPolicy: 'cache-and-network',
-    }),
-    props: ({ data: { loading, thread, fetchMore, refetch } }) => {
-      const loadMoreMessages = () => {
-        if (thread.messages.total > 0) {
-          return fetchMore({
-            variables: {
-              before: thread.messages.data[thread.messages.data.length - 1].id,
-            },
-            updateQuery: (previousResult, { fetchMoreResult }) => {
-              const previousMessages = previousResult.thread.messages.data
-              const currentMessages = fetchMoreResult.thread.messages.data
-
-              return {
-                thread: {
-                  __typename: fetchMoreResult.thread.__typename,
-                  productId: fetchMoreResult.thread.productId,
-                  messages: {
-                    total: fetchMoreResult.thread.messages.total,
-                    __typename: fetchMoreResult.thread.messages.__typename,
-                    data: [...previousMessages, ...currentMessages],
-                  },
-                },
-              }
-            },
-          })
-        }
-      }
-
-      return {
-        data: {
-          loading,
-          thread,
-          refetch,
-          fetchMore: loadMoreMessages,
-        },
-      }
-    },
-  }),
-  graphql(getThreadId, {
-    name: 'threadId',
-    skip: props => {
-      return props.match.params.id !== 'new'
-    },
-    options: props => ({
-      client,
-      variables: {
-        productId: props.location.state.productId,
-      },
-      fetchPolicy: 'cache-and-network',
-    }),
-  }),
-  graphql(getProductQuery, {
-    name: 'product',
-    skip: props => {
-      if (props.match.params.id === 'new') {
-        return false
-      } else if (props.data.thread) {
-        return false
-      }
-
-      return true
-    },
-    options: props => {
-      const id =
-        props.match.params.id === 'new'
-          ? props.location.state.productId
-          : props.data.thread.productId
-      return {
-        variables: {
-          id,
-        },
-      }
-    },
-  }),
-)(Conversation)
+export default Conversation
