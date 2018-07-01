3bc355060ae8cc521ff96fded7de4eaefc4eede0
Yahya Sahaja <yahya.sahaja2@gmail.com>
2018-07-01 21:18:50 +0700

    separate main and vendor

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -76,7 +76,21 @@ module.exports = new Promise(function(resolve) {
       // You can exclude the *.map files from the build during deployment.
       devtool: shouldUseSourceMap ? 'source-map' : false,
       // In production, we only want to load the polyfills and the app code.
-      entry: ['babel-polyfill', require.resolve('./polyfills'), paths.appIndexJs],
+      entry: {
+        main: [
+          'babel-polyfill', 
+          require.resolve('./polyfills'), 
+          paths.appIndexJs,
+        ],
+        vendor: [
+          'react',
+          'react-dom',
+          'react-router-dom',
+          'mobx',
+          'mobx-react',
+          'lodash',
+        ]
+      },
       output: {
         // The build folder.
         path: paths.appBuild,
