b70d9964872f48a93ae22c1ba621ee7f734510c3
Yahya Sahaja <yahya.sahaja2@gmail.com>
2018-11-05 11:41:46 +0700

    coba lg

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -23,7 +23,8 @@ const publicPath = paths.servedPath
 // For these, "homepage" can be set to "." to enable relative asset paths.
 const shouldUseRelativeAssetPaths = publicPath === './'
 // Source maps are resource heavy and can cause out of memory issue for large source files.
-const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false'
+// const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false'
+const shouldUseSourceMap = false
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
@@ -297,7 +298,7 @@ module.exports = new Promise(function(resolve) {
             // https://github.com/facebookincubator/create-react-app/issues/2488
             ascii_only: true,
           },
-          sourceMap: false,
+          sourceMap: shouldUseSourceMap,
         }),
         // Note: this won't work without ExtractTextPlugin.extract(..) in `loaders`.
         new ExtractTextPlugin({
