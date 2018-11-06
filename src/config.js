e01722860099c10412eb977e252d2be0afcae1b8
Yahya Sahaja <yahya.sahaja2@gmail.com>
2018-11-06 23:44:29 +0700

    deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const DOMAIN_URL = 'http://localhost:5000'
+const test = false
+export const DOMAIN_URL = test ? 'http://localhost:5000' : 'https://api.blackbook.ngopi.men'
 export const ENDPOINT_URL = `${DOMAIN_URL}/graphql`
 export const AUTHORIZATION_TOKEN_STORAGE_URI = 'blackbook983uyAuthURI'
 
